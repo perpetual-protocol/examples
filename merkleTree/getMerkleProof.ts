ea3363d02660e4482bc64c847b9594c06cf195c7
Philip Chen <lsps9150414@gmail.com>
2021-10-14 16:27:26 +0800

    refactor: add notes

--- merkleTree/getMerkleProof.ts
+++ merkleTree/getMerkleProof.ts
@@ -2,7 +2,16 @@ import { soliditySha3, toWei } from "web3-utils";
 
 import { loadTree } from "./merkleTree";
 
-export function getClaimWeekContractParams(week, userAddress, balances) {
+export function getClaimWeekContractParams(
+  week: number,
+  userAddress: string,
+  balances: {
+    [address: string]: {
+      /* data */
+    };
+  }
+) {
+  // NOTE: balances: https://s3.amazonaws.com/staking.perp.fi/production/20/0xd6f03f5f7134a2a0d3c831ca7e8243cf88227ceba964345e1ee7ad74b595d526.json
   const claimBalance = balances[userAddress];
   const merkleTree = loadTree(balances);
 
